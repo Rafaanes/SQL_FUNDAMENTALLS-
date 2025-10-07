cd1eed9d0b5d930faf9991b951293fb988d19cd9
Rafaanes <Rafaanes1@hotmail.com>
2025-10-06 21:40:03 -0300

    Primeiro upload
    Esse foi o meu primeiro commit no github

---
+++ Aula01.sql
@@ -0,0 +1,17 @@
+-- Criar comentário
+/*comentario
+de varias
+linhas*/
+-- PascalCase snake_case camelCase --
+CREATE DATABASE db0610_Cadastro;
+
+USE db0610_Cadastro;
+
+-- criando uma tabela de pessoas!
+
+CREATE TABLE tb_alunos(
+	id_aluno INT IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL, 
+	idade 
+	uf
+	);
