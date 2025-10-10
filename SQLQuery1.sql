b2f505d861afefd763d22236c2c623e2ab876b3f
Rafaanes <Rafaanes1@hotmail.com>
2025-10-09 22:11:10 -0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,4 @@
+BACKUP DATABASE [Escola] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\Escola_LogBackup_2025-10-09_21-15-30.bak' 
+WITH NOFORMAT, NOINIT,  NAME = N'Escola-Completo Banco de Dados Backup', 
+SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
--- selects.sql
+++ selects.sql
@@ -85,13 +85,79 @@ SELECT
 FROM Alunos a
 INNER JOIN Notas n ON a.id=n.AlunoId;
 
+---Consultas combinadas (Exists, Union)---
+--[01]Mostrar apenas alunos que tem pagamentos registrados--
+SELECT Nome
+FROM Alunos a
+WHERE EXISTS (
+	SELECT 1 FROM Pagamentos p WHERE p.Alunoid=a.id
+	);
 
+--[02]Combinar os alunos e professores em uma mesma lista--
+SELECT Nome, 'Aluno' AS TIPO
+FROM Alunos 
+UNION
+SELECT Nome, 'Professor' AS TIPO
+FROM Professores;
 
+--[03]Extra Receita final(Alunos com suas ultimas notas)--
+SELECT a.Nome,c.NomeCurso,n.Nota,n.DataAvaliacao
+FROM Alunos a
+INNER JOIN Notas n ON a.id=n.AlunoId
+INNER JOIN Cursos c ON c.id=n.CursoId
+WHERE n.DataAvaliacao = (
+	SELECT MAX(DataAvaliacao)
+	FROM Notas n2
+	WHERE n2.AlunoId=a.id
+	);
 
+---Função de Janela---
+--[01]Ranking de alunos pela nota--
 
+SELECT
+	a.Nome,
+	c.NomeCurso,
+	n.Nota,
+	RANK() OVER (ORDER BY n.Nota DESC)AS Ranking
+FROM Notas n
+JOIN Alunos a ON n.AlunoId=a.id
+JOIN Cursos c ON n.CursoId=c.id
+ORDER BY a.Nome;
 
+---Multiplos calculos---
+--[01] Media, Menor, Maior e Quantidade de notas--
 
+SELECT
+	a.Nome AS Aluno,
+	AVG(CAST(n.Nota AS DECIMAL(10,2))) AS 'Media de Notas',
+	MIN(n.Nota) AS 'Menor Nota',
+	MAX(n.Nota) AS 'Maior Nota',
+	COUNT(*) AS 'Qtd Notas'
+FROM Alunos a
+INNER JOIN  Notas n ON n.AlunoId=a.id
+GROUP BY a.Nome;
 
+--[02]Contar quantas avaliações ha por situação--
+SELECT
+CASE
+	WHEN n.Nota >=7 THEN 'Aprovado'
+	WHEN n.Nota >=5 THEN 'Recuperação'
+	ELSE 'Reprovado'
+END AS Situacao,
+COUNT(*) AS	'Quantidade'
+FROM Alunos a
+INNER JOIN Notas n ON a.id=n.AlunoId
+GROUP BY 
+	CASE
+	WHEN n.Nota >=7 THEN 'Aprovado'
+	WHEN n.Nota >=5 THEN 'Recuperação'
+	ELSE 'Reprovado'
+END
+ORDER BY 'Quantidade' DESC;
 
-
-
+--[03]Apenas alunos com email @gmail.com--
+SELECT 
+	Nome,
+	Email
+FROM Alunos
+	WHERE Email LIKE '%@gmail.com';	
